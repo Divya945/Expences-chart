578e1cd05468e92ebb804936eb7fa7a71cb8b67a
Divya945 <divyadvs945@gmail.com>
2022-05-19 16:07:36 +0530

    1 file modified, 1 added

---
+++ src/Component/ItemExpences.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ItemExpences() {
+  return (
+    <div>
+      <div>March 28 2021</div>
+      <div>
+        <h2>Bike Insurence</h2>
+        <p>2250</p>
+      </div>
+    </div>
+  );
+}
